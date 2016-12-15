b472c09f259c365d02511a7c63e71034e4e52dd7
YU <S3310304067@gms.nutc.edu.tw>
2016-12-15 15:36:38 +0800

    Convert

--- 04-Constant/src/Constant.java
+++ 04-Constant/src/Constant.java
@@ -12,11 +12,11 @@ public class Constant {
         System.out.println("@@@@@@@@@@@@@@@@@");
         //fix #3  將下ㄧ行恢復成註解
         //fix 練習ㄧ #1 將下ㄧ行註解取消, 有問題嗎?
-        MY_PI = 3.14;
+        // MY_PI = 3.14;								// The final local variable MY_PI may already have been assigned
         
         System.out.println("?????????????????");
         //fix #5  如何修改下ㄧ行, 讓 練習四 的下ㄧ行有錯?
-        int X = 982;
+        final int X = 982;
         System.out.println(X);
         System.out.println("$$$$$$$$$$$$$$$$$");
         //fix #6 將下ㄧ行恢復成註解
---
+++ 05-Convert/src/Convert.java
@@ -0,0 +1,19 @@
+
+public class Convert {
+
+	public static void main(String[] args) {
+
+		System.out.println("#####################");
+        double DD  ;
+        DD = 2.5;
+        System.out.println( DD );
+        System.out.println("#####################");
+        //fix #3  如何修改下ㄧ行, 讓 10 除以 4 的較精準的商還是可以存到 DD
+        //fix 練習ㄧ #1 將下ㄧ行註解取消, 有問題嗎?
+        DD = 10 / 4.0;
+        System.out.println( DD );
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println();
+	}
+	
+}
