398f30809206abba216aef4d39e0fcd412add3c0
YU <S3310304067@gms.nutc.edu.tw>
2016-12-15 15:17:12 +0800

    Constant

--- 03-Literal/src/Literal.java
+++ 03-Literal/src/Literal.java
@@ -2,7 +2,27 @@
 public class Literal {
 
 	public static void main(String[] args) {
-		System.out.println( 1234128181281L );
+		System.out.println(1234128181281L); 	// int後面寫L/l 轉型為 long
+		float fv = 7.89f; 						// 加f/F 不加為Double
+		System.out.println(fv);
+
+		// fix #1 將下ㄧ行註解取消, 如何印出 45600000000 的值
+		// System.out.println( 45600000000 );
+		// fix #2 將下兩行註解取消, 有問題嗎, 若有問題怎麼辦?
+		// float FV = 7.89;
+		// System.out.println(FV);
+		// fix #3 觀察下ㄧ行
+		System.out.println("It's a problem");
+		char p;
+		// fix #4 修改下ㄧ行 如何設變數 p 的值為 '
+		p = 'x';
+		System.out.println(p);
+		// fix #5 將下ㄧ行註解取消, 有問題嗎, 若有問題怎麼辦?
+		// 如何印出 "autoexec.bat" 的值
+		// System.out.println(""autoexec.bat"");
+		
+		System.out.println();
+
 	}
 
 }
---
+++ 04-Constant/src/Constant.java
@@ -0,0 +1,28 @@
+
+public class Constant {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+        // fix #2 如何修改下ㄧ行, 讓 練習ㄧ 的下ㄧ行有錯?
+        final double MY_PI;
+        MY_PI = 3.14;
+        System.out.println("#################");
+        System.out.println(MY_PI);
+        System.out.println("@@@@@@@@@@@@@@@@@");
+        //fix #3  將下ㄧ行恢復成註解
+        //fix 練習ㄧ #1 將下ㄧ行註解取消, 有問題嗎?
+        MY_PI = 3.14;
+        
+        System.out.println("?????????????????");
+        //fix #5  如何修改下ㄧ行, 讓 練習四 的下ㄧ行有錯?
+        int X = 982;
+        System.out.println(X);
+        System.out.println("$$$$$$$$$$$$$$$$$");
+        //fix #6 將下ㄧ行恢復成註解
+        //fix 練習四 #4 將下ㄧ行註解取消, 有問題嗎?
+        // X = 982;
+
+	}
+
+}
